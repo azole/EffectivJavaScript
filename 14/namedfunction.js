1154305e5d4df6e3be93b0f2e0385e30b7971dc3
azole <azole@pchome.com.tw>
2013-08-16 16:45:31 +0800

    add item 14

---
+++ 14/namedfunction.js
@@ -0,0 +1,10 @@
+// var constructor = function(){return null};
+// var f = function f(){
+// 	return constructor();
+// };
+
+// console.log(f());
+
+var f = function g(){return 17;};
+console.log(f());
+//console.log(g());	// 在標準環境下是不合法的
