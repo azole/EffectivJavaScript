8f5f08974f6df70b9fa1ef4068a5d3e85f11733f
cyrilhong <cyrilhong@gmail.com>
2013-09-02 14:33:49 +0800

    25 upload

---
+++ 25/bind.js
@@ -0,0 +1,33 @@
+var buffer={
+	entries:[],
+	add: function(s){
+		this.entries.push(s);
+	},
+	concat:function(){
+		return this.entries.join("");
+	}
+};
+方法零
+var source=["867","-","5309"];
+console.log(source.forEach(buffer.add));
+
+
+// 方法一
+// var source=["856","-","5396"];
+// source.forEach(buffer.add, buffer);
+// console.log(buffer.contact());
+
+// 方法二
+// var source=["856","-","5396"];
+// source.forEach(function(s){
+// 	buffer.add(s);
+// });
+// console.log(buffer.contact());
+
+// 使用bind
+// var source=["856","-","5396"];
+// source.forEach(buffer.add.bind(buffer));
+// console.log(buffer.contact());
+
+// 結論
+// buffer.add != buffer.add.binf(buffer);
